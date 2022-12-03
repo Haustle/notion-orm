478a2225c0e20a83d39227baeb1702539f52bf93
Tyrus <tyrus.miles@gmail.com>
2022-12-02 23:03:16 -0500

    add dotenv to notion-collection

--- src/NotionActions/NotionCollection.ts
+++ src/NotionActions/NotionCollection.ts
@@ -2,57 +2,58 @@ import { CreatePageParameters } from "@notionhq/client/build/src/api-endpoints";
 import { Client } from "@notionhq/client";
 import { getCall } from "./BuildCall";
 import { PropertyType } from "./GenerateTypes";
+require("dotenv").config();
 
 interface Query<T> {
-    query: {
-        where: Partial<T>;
-    };
+	query: {
+		where: Partial<T>;
+	};
 }
 type propNameToColumnNameType = Record<
-    string,
-    { columnName: string; type: PropertyType }
+	string,
+	{ columnName: string; type: PropertyType }
 >;
 
 export class CollectionActions<CollectionType extends {}> {
-    private databaseId: string;
-    private propNameToColumnName: propNameToColumnNameType;
+	private databaseId: string;
+	private propNameToColumnName: propNameToColumnNameType;
 
-    constructor(
-        datbaseId: string,
-        propNameToColumnName: propNameToColumnNameType
-    ) {
-        this.databaseId = datbaseId;
-        this.propNameToColumnName = propNameToColumnName;
-    }
+	constructor(
+		datbaseId: string,
+		propNameToColumnName: propNameToColumnNameType
+	) {
+		this.databaseId = datbaseId;
+		this.propNameToColumnName = propNameToColumnName;
+	}
 
-    // Add page to a database
-    async add(pageObject: CollectionType) {
-        const NotionClient: Client = new Client({
-            auth: process.env.NOTION_KEY
-        });
-        const callBody: CreatePageParameters = {
-            parent: {
-                database_id: this.databaseId,
-            },
-            properties: {},
-        };
+	// Add page to a database
+	async add(pageObject: CollectionType) {
+		const NotionClient: Client = new Client({
+			auth: process.env.NOTION_KEY,
+		});
+		const callBody: CreatePageParameters = {
+			parent: {
+				database_id: this.databaseId,
+			},
+			properties: {},
+		};
 
-        const columnTypePropNames = Object.keys(pageObject);
-        columnTypePropNames.forEach((propName) => {
-            const { type, columnName } = this.propNameToColumnName[propName];
-            const columnObject = getCall({
-                type,
-                // @ts-ignore
-                value: pageObject[propName],
-            });
+		const columnTypePropNames = Object.keys(pageObject);
+		columnTypePropNames.forEach((propName) => {
+			const { type, columnName } = this.propNameToColumnName[propName];
+			const columnObject = getCall({
+				type,
+				// @ts-ignore
+				value: pageObject[propName],
+			});
 
-            callBody.properties[columnName] = columnObject!;
-        });
+			callBody.properties[columnName] = columnObject!;
+		});
 
-        // console.log(JSON.stringify(callBody, null, 4));
-        await NotionClient.pages.create(callBody);
-    }
+		// console.log(JSON.stringify(callBody, null, 4));
+		await NotionClient.pages.create(callBody);
+	}
 
-    // Look for page inside the database
-    query(q: Query<CollectionType>) {}
+	// Look for page inside the database
+	query(q: Query<CollectionType>) {}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,18 @@
 import "module-alias/register";
+import { createDatabaseTypes } from "./NotionActions/NotionConfig";
 import { notion } from "./NotionActions/notion";
-import { getDatabaseTypes } from "./NotionActions/NotionConfig";
-import "dotenv";
 
 async function main() {
-    const fascinationIds = "d623506cf7584f7cb5350f363301169a";
+	// createDatabaseTypes({
+	// 	auth: process.env.NOTION_KEY ?? "",
+	// 	databaseIds: ["a52239e4839d4a3a8f4875376cfbfb02"],
+	// });
 
-    getDatabaseTypes({
-        auth: process.env.NOTION_KEY ?? "",
-        databaseIds: [fascinationIds, "f6d58ae659a942178efeb13fddab0e25"],
-    });
-
-    // notion.tyrusFascinations.add({
-    //     name: "steve jobs x joe rogan",
-    //     mediaType: "Podcast",
-    //     discussion: ["AI", "Coding"],
-    // });
+	notion.dimitriSFightingVideo.add({
+		combo: "some late combo",
+		character: ["Joker", "Kid buu"],
+		game: "smash",
+	});
 }
 
 main();
