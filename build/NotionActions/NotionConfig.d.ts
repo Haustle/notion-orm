01f6046c33247771eb7047639ba4868805c5fb47
Tyrus <tyrus.miles@gmail.com>
2022-12-16 23:59:36 -0500

    Revert "pushing build files"
    This reverts commit 1a65f3242390aa75702eee3c106f45a4045dc138.

--- build/NotionActions/NotionConfig.d.ts
+++
@@ -1,8 +0,0 @@
-export type NotionConfigType = {
-    auth: string;
-    databaseIds: string[];
-};
-export declare const createDatabaseTypes: (notionInfo: NotionConfigType) => Promise<{
-    databaseNames: string[];
-}>;
-//# sourceMappingURL=NotionConfig.d.ts.map
