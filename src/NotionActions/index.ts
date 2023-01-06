b7b67ef3cd5399ca1225717e5fc1fe31662d9cef
Tyrus <tyrus.miles@gmail.com>
2023-01-05 23:34:34 -0500

    updated cli command; moved index.ts to NotionActions

--- src/NotionActions/index.ts
+++ src/NotionActions/index.ts
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
 
 import fs from "fs";
-import { createDatabaseTypes } from "./NotionActions/NotionConfig";
+import { createDatabaseTypes } from "./NotionConfig";
 import path from "path";
 require("dotenv").config();
 
